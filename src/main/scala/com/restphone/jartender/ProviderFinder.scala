fb59211d67e14e49f933c3d4516112f6ec8cd195
James Moore <james@restphone.com>
2012-11-10 18:12:55 -0800

    type changes

--- src/main/scala/com/restphone/jartender/ProviderFinder.scala
+++ src/main/scala/com/restphone/jartender/ProviderFinder.scala
@@ -13,40 +13,36 @@ import org.objectweb.asm.Handle
 import org.objectweb.asm.AnnotationVisitor
 import org.objectweb.asm.Label
 
-trait IdentifierFlavor extends Any
+trait IdentifierFlavor extends Any {
+  def usesClasses: Set[UsesClass]
+}
 
-case class InternalName(s: String) extends {
+case class InternalName(s: String) extends AnyVal with IdentifierFlavor {
   def javaIdentifier = JavaIdentifier(s.replace("/", "."))
+  def usesClasses = javaIdentifier.usesClasses
 }
 case class MethodDescriptor(s: String) extends AnyVal with IdentifierFlavor {
   def usesClasses = ProviderFinder.methodDescriptorToUsesClass(this)
 }
-case class TypeDescriptor(s: String) extends AnyVal with IdentifierFlavor
-case class JavaIdentifier(s: String) extends AnyVal with IdentifierFlavor
+case class TypeDescriptor(s: String) extends AnyVal with IdentifierFlavor {
+  def usesClasses = ProviderFinder.typeDescriptorToUsesClass(this)
+}
+case class JavaIdentifier(s: String) extends AnyVal with IdentifierFlavor {
+  def usesClasses = Set(UsesClass(this))
+}
 case class Signature(s: String) extends AnyVal with IdentifierFlavor
-
-trait ProvidesInternalName {
-  def internalName: InternalName
+object OptionalSignature {
+  def apply(s: String) = s match {
+    case null => None
+    case s => some(Signature(s))
+  }
 }
-trait ProvidesJavaIdentifier {
-  def javaIdentifer: JavaIdentifier
-}
-trait ProvidesTypeDescriptor {
+trait UsesClassesIsBuiltFromTypeDescriptor {
   def typeDescriptor: TypeDescriptor
+  def usesClasses = typeDescriptor.usesClasses
 }
-trait ProvidesJavaIdentifierFromInternalName extends ProvidesJavaIdentifier with ProvidesInternalName {
-  def javaIdentifier = internalName.javaIdentifier
-}
-trait UsesClassFromJavaIdentifier extends ProvidesJavaIdentifier {
-  def usesClasses = Set(JavaIdentifier)
-}
-trait UsesClassFromTypeDescriptor extends ProvidesTypeDescriptor {
-    def usesClasses = ProviderFinder.typeDescriptorToUsesClass(typeDescriptor)
-}
-
 sealed abstract class Provider {
   def usesClasses: Set[UsesClass]
-  def javaIdentifiersToUsesClass(xs: Iterable[JavaIdentifier]) = xs map {UsesClass(_)} toSet
 }
 
 /**
@@ -59,21 +55,22 @@ case class ProvidesClass(
   internalName: InternalName,
   signature: Option[Signature],
   superName: InternalName,
-  interfaces: List[InternalName]) extends Provider with ProvidesJavaIdentifierFromInternalName {
+  interfaces: List[InternalName]) extends Provider {
+  import scalaz._
+  import Scalaz._
   // Note that interfaces are classes with access bits of ACC_INTERFACE and ACC_ABSTRACT set (0x400, 0x200)
   //  def field(access: Int, name: String, desc: String, signature: String, value: Object, annotations: List[UsesClass]) = ProvidesField(access, name, desc, Option(signature), value)
   //  def method(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]) = ProvidesMethod(access, name, desc, Option(signature), exceptions.toList)
-  val items = internalName :: interfaces
-  def usesClasses = items map { i => UsesClass(i.javaIdentifier) } toSet
+  val javaIdentifier = internalName.javaIdentifier;
+  val internalNames = internalName :: interfaces
+  def usesClasses = ProviderFinder.convertListOfProviderToUsesClasses(internalName :: interfaces)
 }
 object ProvidesClass {
   def createProvidesClassMatcher(fn: ProvidesClass => Boolean) = new Object {
     def unapply(f: ProvidesClass) = fn(f)
   }
 }
-case class ProvidesField(access: Int, name: InternalName, desc: TypeDescriptor, signature: Option[Signature], value: Option[Object]) extends Provider {
-  def usesClasses = ProviderFinder.typeDescriptorToUsesClass(desc)
-}
+case class ProvidesField(access: Int, name: InternalName, typeDescriptor: TypeDescriptor, signature: Option[Signature], value: Option[Object]) extends Provider with UsesClassesIsBuiltFromTypeDescriptor
 object ProvidesField {
   def createProvidesFieldMatcher(fn: ProvidesField => Boolean) = new Object {
     def unapply(f: ProvidesField) = fn(f)
@@ -85,34 +82,27 @@ case class ProvidesMethod(
   desc: MethodDescriptor,
   signature: Option[Signature],
   exceptions: List[InternalName]) extends Provider {
-  def usesClasses = {
-    val descriptorTypes = ProviderFinder.methodDescriptorToUsesClass(desc)
-    val exceptionTypes = exceptions map { _.javaIdentifier } map UsesClass
-    descriptorTypes ++ exceptionTypes
-  }
+  def usesClasses = ProviderFinder.convertListOfProviderToUsesClasses(desc :: exceptions)
 }
-case class UsesClass(javaIdentifier: JavaIdentifier) extends Provider with UsesClassFromJavaIdentifier
+case class UsesClass(javaIdentifier: JavaIdentifier) extends Provider {
+  def usesClasses = Set(this)
+}
 
-case class UsesAnnotation(typeDescriptor: TypeDescriptor, visibleAtRuntime: Option[Boolean]) extends Provider {
-  def usesClasses = ProviderFinder.typeDescriptorToUsesClass(typeDescriptor)
-}
+case class UsesAnnotation(typeDescriptor: TypeDescriptor, visibleAtRuntime: Option[Boolean]) extends Provider with UsesClassesIsBuiltFromTypeDescriptor
+
 case class UsesAnnotationArray(name: String) extends Provider {
   def usesClasses = Set.empty
 }
-case class UsesAnnotationEnum(name: Option[String], typeDescriptor: TypeDescriptor, value: String) extends Provider {
-  def usesClasses = ProviderFinder.typeDescriptorToUsesClass(typeDescriptor)
+case class UsesAnnotationEnum(name: Option[String], typeDescriptor: TypeDescriptor, value: String) extends Provider with UsesClassesIsBuiltFromTypeDescriptor
+case class UsesParameterAnnotation(descriptor: TypeDescriptor) extends Provider with UsesClassesIsBuiltFromTypeDescriptor
+case class UsesMethod(opcode: Int, owner: TypeDescriptor, name: String, desc: MethodDescriptor) extends Provider {
+  def usesClasses = ProviderFinder.convertListOfProviderToUsesClasses(List(desc, owner))
 }
-case class UsesParameterAnnotation(descriptor: String) extends Provider {
-  def usesClasses = ProviderFinder.typeDescriptorToUsesClass(descriptor)
+case class UsesField(opcode: Int, owner: InternalName, name: String, desc: TypeDescriptor) extends Provider {
+  def usesClasses = ProviderFinder.convertListOfProviderToUsesClasses(List(desc, owner))
 }
-case class UsesMethod(opcode: Int, owner: String, name: String, desc: String) extends Provider {
-  def usesClasses = ProviderFinder.methodDescriptorToUsesClass(desc) ++ ProviderFinder.internalNamesToUsesClass(List(owner))
-}
-case class UsesField(opcode: Int, owner: String, name: String, desc: String) extends Provider {
-  def usesClasses = ProviderFinder.typeDescriptorToUsesClass(desc) ++ ProviderFinder.internalNamesToUsesClass(List(owner))
-}
-case class UsesException(exceptionType: String) extends Provider {
-  def usesClasses = Set(ProviderFinder.internalNameToUsesClass(exceptionType))
+case class UsesException(exceptionType: InternalName) extends Provider {
+  def usesClasses = exceptionType.usesClasses
 }
 
 case class ProviderFinder extends org.objectweb.asm.ClassVisitor(Opcodes.ASM4) {
@@ -135,58 +125,58 @@ case class ProviderFinder extends org.objectweb.asm.ClassVisitor(Opcodes.ASM4) {
     val cls = ProvidesClass(
       version = version,
       access = access,
-      internalName = name,
-      signature = Option(signature),
-      superName = superName,
-      interfaces = nullToEmptyList(interfaces))
+      internalName = InternalName(name),
+      signature = OptionalSignature(signature),
+      superName = InternalName(superName),
+      interfaces = nullToEmptyList(interfaces) map InternalName)
     elements.push(cls)
   }
 
-  def pushAnnotationAndReturnANewVisitor(desc: String, visibleAtRuntime: Option[Boolean], usesGenerator: (String, Option[Boolean]) => Provider = { (n, v) => UsesAnnotation(n, v) }): AnnotationVisitor = {
+  def pushAnnotationAndReturnANewVisitor(desc: TypeDescriptor, visibleAtRuntime: Option[Boolean], usesGenerator: (TypeDescriptor, Option[Boolean]) => Provider = { (n, v) => UsesAnnotation(n, v) }): AnnotationVisitor = {
     elements.push(usesGenerator(desc, visibleAtRuntime))
     new AnnotationVisitor(Opcodes.ASM4) {
-      override def visitAnnotation(name: String, desc: String): AnnotationVisitor = pushAnnotationAndReturnANewVisitor(desc, visibleAtRuntime)
-      override def visitArray(name: String) = pushAnnotationAndReturnANewVisitor("", None, { (_, _) => UsesAnnotationArray(name) })
-      override def visitEnum(name: String, desc: String, value: String) = elements.push(UsesAnnotationEnum(Option(name), desc, value))
+      override def visitAnnotation(name: String, desc: TypeDescriptor): AnnotationVisitor = pushAnnotationAndReturnANewVisitor(desc, visibleAtRuntime)
+      override def visitArray(name: String) = pushAnnotationAndReturnANewVisitor(TypeDescriptor(""), None, { (_, _) => UsesAnnotationArray(name) })
+      override def visitEnum(name: String, desc: TypeDescriptor, value: String) = elements.push(UsesAnnotationEnum(Option(name), desc, value))
     }
   }
 
   override def visitField(access: Int, name: String, desc: String, signature: String, value: Object) = {
-    val f = ProvidesField(access, name, desc, Option(signature), Option(value))
+    val f = ProvidesField(access, InternalName(name), TypeDescriptor(desc), OptionalSignature(signature), Option(value))
     elements.push(f)
     new FieldVisitor(Opcodes.ASM4) {
-      override def visitAnnotation(desc: String, visible: Boolean) = pushAnnotationAndReturnANewVisitor(desc, some(visible))
+      override def visitAnnotation(desc: String, visible: Boolean) = pushAnnotationAndReturnANewVisitor(TypeDescriptor(desc), some(visible))
     }
   }
 
   override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]) = {
     //    currentClassProvider = currentClassProvider.head.method( access, name, desc, signature, exceptions ) :: currentClassProvider.tail
-    elements.push(ProvidesMethod(access, name, desc, Option(signature), nullToEmptyList(exceptions)))
+    elements.push(ProvidesMethod(access, JavaIdentifier(name), MethodDescriptor(desc), OptionalSignature(signature), nullToEmptyList(exceptions) map InternalName))
 
     new MethodVisitor(Opcodes.ASM4) {
       override def visitAnnotation(desc: String, visible: Boolean) =
-        pushAnnotationAndReturnANewVisitor(desc, some(visible))
+        pushAnnotationAndReturnANewVisitor(TypeDescriptor(desc), some(visible))
 
       override def visitParameterAnnotation(parameter: Int, desc: String, visible: Boolean) =
-        pushAnnotationAndReturnANewVisitor(desc, some(visible), (name, _) => UsesParameterAnnotation(name))
+        pushAnnotationAndReturnANewVisitor(TypeDescriptor(desc), some(visible), (name, _) => UsesParameterAnnotation(name))
 
       //     public void visitEnum(String name, String desc, String value) {
 
       override def visitFieldInsn(opcode: Int, owner: String, name: String, desc: String) =
-        elements.push(UsesField(opcode, owner, name, desc))
+        elements.push(UsesField(opcode, InternalName(owner), name, TypeDescriptor(desc)))
 
       override def visitMethodInsn(opcode: Int, owner: String, name: String, desc: String) =
-        elements.push(UsesMethod(opcode, owner, name, desc))
+        elements.push(UsesMethod(opcode, TypeDescriptor(owner), name, MethodDescriptor(desc)))
 
       override def visitTryCatchBlock(start: Label, end: Label, handler: Label, exceptionType: String) =
-        elements.push(UsesException(exceptionType))
+        elements.push(UsesException(InternalName(exceptionType)))
 
       override def visitInvokeDynamicInsn(name: String, desc: String, bsm: Handle, bsmArgs: Object*) =
         null
     }
   }
 
-  override def visitAnnotation(desc: String, visible: Boolean) = pushAnnotationAndReturnANewVisitor(desc, some(visible))
+  override def visitAnnotation(desc: String, visible: Boolean) = pushAnnotationAndReturnANewVisitor(TypeDescriptor(desc), some(visible))
 }
 
 object ProviderFinder {
@@ -225,6 +215,6 @@ object ProviderFinder {
   def parseResultUsesClass(fn: { def typesUsed: Set[JavaName] }) = fn.typesUsed map { _.toJava } map JavaIdentifier map UsesClass
   def typeDescriptorToUsesClass(descriptor: TypeDescriptor): Set[UsesClass] = parseResultUsesClass(JavaSignatureParser.parse(descriptor.s).get)
   def methodDescriptorToUsesClass(descriptor: MethodDescriptor): Set[UsesClass] = parseResultUsesClass(JavaSignatureParser.parseMethod(descriptor.s).get)
-
+  def convertListOfProviderToUsesClasses[T <% Provider](xs: Iterable[IdentifierFlavor]) = xs map { _.usesClasses } reduce (_ ++ _)
   def extractClasses(p: Provider): Set[UsesClass] = p.usesClasses
 }
