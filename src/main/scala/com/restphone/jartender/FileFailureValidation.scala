3e626f8c8142bbdd0879dad1fb08ccc0b92b2e63
James Moore <james@restphone.com>
2012-12-16 17:49:31 -0800

    Added more file validation tools

--- src/main/scala/com/restphone/jartender/FileFailureValidation.scala
+++ src/main/scala/com/restphone/jartender/FileFailureValidation.scala
@@ -14,11 +14,25 @@ import scala.util.control.Exception._
 import java.io.IOException
 
 object FileFailureValidation {
+  case class FileFailure( context: String, errorMessage: String )
+  type FileFailureValidation[T] = ValidationNEL[FileFailure, T]
+
   def convertIoExceptionToValidation( context: String ) = {
     val a = catching( classOf[IOException] ) withApply { x => ( FileFailure( context, x.getLocalizedMessage ) ).failNel }
     val b = catching( classOf[RuntimeException] ) withApply { x => ( FileFailure( context, x.getLocalizedMessage ) ).failNel }
     a or b
   }
-  case class FileFailure( context: String, errorMessage: String )
-  type FileFailureValidation[T] = ValidationNEL[FileFailure, T]
+
+  def validDirectory( f: File, failureMsg: String ): FileFailureValidation[File] =
+    if ( f.isDirectory ) f.success else FileFailure( failureMsg, f.getPath + " is not a valid directory" ).failureNel
+
+  def validatedFile( f: File, failureMsg: String ): FileFailureValidation[File] =
+    if ( f.isFile && f.canRead && f.length > 0 ) f.success else FileFailure( failureMsg, f.getPath + " is not readable with length > 0" ).failureNel
+
+  def validatedTempFile( validationContext: String, prefix: String, suffix: String, directory: File ): FileFailureValidation[File] = {
+    for {
+      dir <- validDirectory( directory, validationContext )
+      tmpfile <- convertIoExceptionToValidation( validationContext ) { File.createTempFile( prefix, suffix, dir ).success }
+    } yield tmpfile
+  }
 }
